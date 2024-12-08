97cd142286375f65d67ec659f5cb6a5feb2ff024
Dmitry <kurson.d@gmail.com>
2024-12-08 23:54:22 +0200

    Added additional folders, contorllers

---
+++ MyShop/Controllers/ProducerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyShop.Controllers
+{
+    public class ProducerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
